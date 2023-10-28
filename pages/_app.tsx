a674bc39888e0e4f6359a203e4dd56247afe4d71
moganesan <developer.moganesan>
2023-10-28 10:54:11 +0530

    react context wrapped into main component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { WalletsContextProvider } from "@/context/walletsContext";
+import { NetworkContextProvider } from "@/context/networkContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NetworkContextProvider>
+      <WalletsContextProvider>
+        <Component {...pageProps} />
+      </WalletsContextProvider>
+    </NetworkContextProvider>
+  );
 }
