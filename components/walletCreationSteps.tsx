5009fbb48bab88f2ba00577cb5bdaefaa16f8faa
moganesan <developer.moganesan>
2023-10-28 17:02:09 +0530

    face recognition added to wallet creation compoennt

--- components/walletCreationSteps.tsx
+++ components/walletCreationSteps.tsx
@@ -1,6 +1,12 @@
-import { useState, Fragment } from "react";
+import { useState, Fragment, useEffect, useCallback } from "react";
 import { Transition } from "@headlessui/react";
-import { setWalletPassword } from "@/helpers/wallet";
+import { decryptPrivateKey, setWalletPassword } from "@/helpers/wallet";
+import "@anima-protocol/personhood-sdk-react/style.css";
+import { useRouter } from "next/router";
+import { Personhood } from "@anima-protocol/personhood-sdk-react";
+import { useWalletsContext } from "@/context/walletsContext";
+import { requestAsyncStorage } from "next/dist/client/components/request-async-storage.external";
+import { ethers, Wallet } from "ethers";
 
 function WalletCreationStepper() {
   const [currentStep, setCurrentStep] = useState(1);
@@ -8,6 +14,69 @@ function WalletCreationStepper() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [recoveryPhrase, setRecoveryPhrase] = useState("");
   const [verifiedRecoveryPhrase, setVerifiedRecoveryPhrase] = useState("");
+  const [sessionId, setSessionId] = useState("");
+
+  const router = useRouter();
+
+  const { selectedWallet } = useWalletsContext();
+
+  useEffect(() => {
+    initSession();
+  }, []);
+
+  // useEffect(() => {
+  //   fetchSessionDetails();
+  // }, [sessionId]);
+  const fetchSessionDetails = async () => {
+    if (!sessionId) return;
+    const myHeaders = new Headers();
+    myHeaders.append(
+      "Api-Key",
+      "test_4ui5nnVlg5ZylpDPaeVfREUD4N5Ogt4zjXAQzVf7ZGelE4W9r1TSjb1SfHzYKqww"
+    );
+    const requestOptions: any = {
+      method: "POST",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+    try {
+      const response = await fetch(
+        `https://api.pop.anima.io/v1/sessions/${sessionId}/details`,
+        requestOptions
+      );
+      console.log("res1", response);
+      const result = await response.json();
+      console.log("res1", result);
+    } catch (error) {
+      console.log("Fetch session details error", error);
+    }
+  };
+
+  const initSession = async () => {
+    if (sessionId) return;
+    const myHeaders = new Headers();
+    myHeaders.append(
+      "Api-Key",
+      "test_4ui5nnVlg5ZylpDPaeVfREUD4N5Ogt4zjXAQzVf7ZGelE4W9r1TSjb1SfHzYKqww"
+    );
+    const requestOptions: any = {
+      method: "POST",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+    try {
+      const response = await fetch(
+        "https://api.pop.anima.io/v1/personhood/init",
+        requestOptions
+      );
+      console.log("res", response);
+      const result = await response.json();
+      console.log("res", result.session_id);
+      setSessionId(result?.session_id); // Assuming the response contains a field named 'sessionId'
+    } catch (error) {
+      console.log("Initialization error", error);
+    }
+  };
 
   const nextStep = () => {
     setCurrentStep(currentStep + 1);
@@ -34,6 +103,31 @@ function WalletCreationStepper() {
 
   const handleVerifiedRecoveryPhraseChange = (event: any) => {
     setVerifiedRecoveryPhrase(event.target.value);
+  };
+
+  const shared = useCallback((e: { info: string }) => {
+    console.log("shared", e.info);
+  }, []);
+
+  const sign = async (payload: string) => {
+    const privateKey = decryptPrivateKey(
+      password,
+      selectedWallet?.privateKey,
+      selectedWallet?.salt,
+      selectedWallet?.iv,
+      selectedWallet?.tag
+    );
+
+    if (privateKey == false) alert("Decryption Erro");
+
+    console.log("private key", privateKey);
+    const wallet = new Wallet(privateKey.toString());
+
+    // Sign the message using ethers.js
+    const signature = wallet.signMessage(payload);
+
+    // Return the signature
+    return signature;
   };
 
   const renderStep = () => {
@@ -73,33 +167,33 @@ function WalletCreationStepper() {
         );
       case 2:
         return (
-          <div>
-            <h2>Step 2: Get Recovery Phrase</h2>
-            {/* Display recovery phrase and instructions here */}
-            <button onClick={nextStep}>Next</button>
+          <div className="text-center px-10 flex flex-col items-center">
+            <h2 className="font-bold text-xl">Face Authentication</h2>
+            <p className="font-light mt-2">
+              Verify your face in the decentralized world.
+            </p>
+            <div className="mt-4">
+              <Personhood
+                onFinish={shared}
+                sessionId={sessionId}
+                signCallback={sign}
+                walletAddress={selectedWallet?.address}
+              />
+            </div>
+            <button
+              onClick={() => router.push("/")}
+              className="w-full bg-blue-500 h-14 rounded-md mt-10"
+            >
+              Go To Dashboard
+            </button>
           </div>
         );
-      case 3:
-        return (
-          <div>
-            <h2>Step 3: Verify Recovery Phrase</h2>
-            <input
-              type="text"
-              value={verifiedRecoveryPhrase}
-              onChange={handleVerifiedRecoveryPhraseChange}
-            />
-            <button onClick={prevStep}>Previous</button>
-            <button onClick={nextStep}>Finish</button>
-          </div>
-        );
-      default:
-        return null;
     }
   };
 
   return (
     <div className="text-center">
-      <h1 className="mb-10 text-4xl font-bold">Step {currentStep} of 3</h1>
+      <h1 className="mb-10 text-4xl font-bold">Step {currentStep} of 2</h1>
       {renderStep()}
     </div>
   );
